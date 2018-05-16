7e108d0132cd848da9cad12e50955ce53241aac3
Enrico Seiler <enrico.seiler@hotmail.de>
2018-05-16 14:19:00 +0200

    Add numDigits

--- include/seqan/kmer/kmer_base.h
+++ include/seqan/kmer/kmer_base.h
@@ -82,6 +82,18 @@ public:
     ~Critical_section() { s.notify(); }
 };
 
+template <typename T>
+int numDigits(T number)
+{
+    int digits = 0;
+    if (number <= 0) digits = 1; // remove this line if '-' counts as a digit
+    while (number) {
+        number /= 10;
+        digits++;
+    }
+    return digits;
+}
+
 // ==========================================================================
 // Tags, Classes, Enums
 // ==========================================================================
