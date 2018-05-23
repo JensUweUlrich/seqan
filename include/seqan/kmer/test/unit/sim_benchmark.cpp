91f1a632f8351724b46e672d31ce2044f601bb51
Enrico Seiler <enrico.seiler@hotmail.de>
2018-05-23 09:58:49 +0200

    Add main()

--- include/seqan/kmer/test/unit/sim_benchmark.cpp
+++ include/seqan/kmer/test/unit/sim_benchmark.cpp
@@ -11,70 +11,73 @@ std::random_device rd;
 std::mt19937 rng(rd());
 std::uniform_real_distribution<double> dist(0, 1);
 
-for(uint16_t noOfBins : {64, 256, 1024, 8192})
+int main()
 {
-    // Since numerator and denominator are powers of two, each bin should get an equal number of reads
-    uint32_t readsPerBin = noOfReads / noOfBins;
-    // Since numerator and denominator are powers of two, each haplotype should get an equal number of reads
-    uint32_t readsPerHaplotype = readsPerBin / noOfHaplotypes;
-
-    for(int32_t bin = 0; bin < noOfBins; ++bin)
+    for(uint16_t noOfBins : {64, 256, 1024, 8192})
     {
-        CharString fileIn(baseDir);
-        append(fileIn, CharString(std::to_string(noOfBins)));
-        append(fileIn, CharString{"/bins/bin_"});
-        append(fileIn, CharString(std::string(numDigits(bins)-numDigits(bin), '0') + (std::to_string(bin))));
-        append(fileIn, CharString(".fasta"));
+        // Since numerator and denominator are powers of two, each bin should get an equal number of reads
+        uint32_t readsPerBin = noOfReads / noOfBins;
+        // Since numerator and denominator are powers of two, each haplotype should get an equal number of reads
+        uint32_t readsPerHaplotype = readsPerBin / noOfHaplotypes;
 
-        CharString fileOut(baseDir);
-        append(fileOut, CharString(std::to_string(noOfBins)));
-        append(fileOut, CharString{"/reads/bin_"});
-        append(fileOut, CharString(std::string(numDigits(bins)-numDigits(bin), '0') + (std::to_string(bin))));
-        append(fileOut, CharString(".fastq"));
-
-        SeqFileIn seqFileIn;
-        if (!open(seqFileIn, toCString(fileIn)))
+        for(int32_t bin = 0; bin < noOfBins; ++bin)
         {
-            CharString msg = "Unable to open contigs file: ";
-            append(msg, CharString(fileIn));
-            std::cerr << msg << '\n';
-            throw toCString(msg);
-        }
+            CharString fileIn(baseDir);
+            append(fileIn, CharString(std::to_string(noOfBins)));
+            append(fileIn, CharString{"/bins/bin_"});
+            append(fileIn, CharString(std::string(numDigits(bins)-numDigits(bin), '0') + (std::to_string(bin))));
+            append(fileIn, CharString(".fasta"));
 
-        SeqFileOut seqFileOut;
-        if (!open(seqFileOut, toCString(fileOut)))
-        {
-            CharString msg = "Unable to open contigs file: ";
-            append(msg, CharString(fileOut));
-            std::cerr << msg << '\n';
-            throw toCString(msg);
-        }
+            CharString fileOut(baseDir);
+            append(fileOut, CharString(std::to_string(noOfBins)));
+            append(fileOut, CharString{"/reads/bin_"});
+            append(fileOut, CharString(std::string(numDigits(bins)-numDigits(bin), '0') + (std::to_string(bin))));
+            append(fileOut, CharString(".fastq"));
 
-        CharString id;
-        String<Dna> seq;
-
-        while(!atEnd(seqFileIn))
-        {
-            readRecord(id, seq, seqFileIn);
-            uint32_t refLength = length(seq);
-            std::uniform_real_distribution<uint32_t> readPos(0, refLength - readLength + 1);
-            for(uint_32_t r = 0; r < readsPerHaplotype; ++r)
+            SeqFileIn seqFileIn;
+            if (!open(seqFileIn, toCString(fileIn)))
             {
-                uint32_t pos = readPos(rng);
-                DnaString segment = infixWithLength(seq, pos, readLength);
-                std::uniform_real_distribution<uint16_t> errorPos(0, readLength);
-                for(uint8_t e = 0; e < maxErrors; ++e)
+                CharString msg = "Unable to open contigs file: ";
+                append(msg, CharString(fileIn));
+                std::cerr << msg << '\n';
+                throw toCString(msg);
+            }
+
+            SeqFileOut seqFileOut;
+            if (!open(seqFileOut, toCString(fileOut)))
+            {
+                CharString msg = "Unable to open contigs file: ";
+                append(msg, CharString(fileOut));
+                std::cerr << msg << '\n';
+                throw toCString(msg);
+            }
+
+            CharString id;
+            String<Dna> seq;
+
+            while(!atEnd(seqFileIn))
+            {
+                readRecord(id, seq, seqFileIn);
+                uint32_t refLength = length(seq);
+                std::uniform_real_distribution<uint32_t> readPos(0, refLength - readLength + 1);
+                for(uint_32_t r = 0; r < readsPerHaplotype; ++r)
                 {
-                    uint32_t pos = errorPos(rng);
-                    Dna currentBase = segment[pos];
-                    Dna newBase = currentBase;
-                    while (newBase == currentBase)
-                        newBase = Dna(static_cast<int>(dist(rng) / 0.25));
-                    segment[pos] = newBase;
-                    if (dist(rng) < 0.5)
-                        break;
+                    uint32_t pos = readPos(rng);
+                    DnaString segment = infixWithLength(seq, pos, readLength);
+                    std::uniform_real_distribution<uint16_t> errorPos(0, readLength);
+                    for(uint8_t e = 0; e < maxErrors; ++e)
+                    {
+                        uint32_t pos = errorPos(rng);
+                        Dna currentBase = segment[pos];
+                        Dna newBase = currentBase;
+                        while (newBase == currentBase)
+                            newBase = Dna(static_cast<int>(dist(rng) / 0.25));
+                        segment[pos] = newBase;
+                        if (dist(rng) < 0.5)
+                            break;
+                    }
+                    writeRecord(seqFileOut, id, segment);
                 }
-                writeRecord(seqFileOut, id, segment);
             }
         }
     }
