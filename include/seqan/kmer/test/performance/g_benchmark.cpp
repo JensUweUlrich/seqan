b1b51b113238a2eb217fa31ade04a5012b622fd8
Enrico Seiler <enrico.seiler@hotmail.de>
2018-05-30 12:14:49 +0200

    apply changes to DA

--- include/seqan/kmer/test/performance/g_benchmark.cpp
+++ include/seqan/kmer/test/performance/g_benchmark.cpp
@@ -277,29 +277,52 @@ static void select_DA(benchmark::State& state)
     auto k = state.range(1);
     KmerFilter<TAlphabet, DirectAddressing, TFilter> da (bins, k);
 
-    uint64_t readNo{1};
-
     CharString storage("");
     append(storage, CharString(std::to_string(bins)));
     append(storage, CharString("_"));
     append(storage, CharString(std::to_string(k)));
-    if constexpr (std::is_same_v<TFilter, Uncompressed>) {
+    append(storage, CharString("_"));
+    append(storage, CharString(std::to_string(bits)));
+    if constexpr (std::is_same_v<TFilter, Uncompressed> || std::is_same_v<TFilter, CompressedSimple>) {
         append(storage, CharString("_Uncompressed"));
     }
     else
     {
-        append(storage, CharString("_Compressed"));
+        throw "Invalid template";
     }
     append(storage, CharString("_da.filter"));
-    retrieve(da, storage);
-    da.filterVector.compress(0);
 
-    uint64_t verifications{0};
-    uint64_t tp{0};
+    auto fullTime = std::chrono::high_resolution_clock::now();
+
+    double loadingTime{0.0};
+    auto start = std::chrono::high_resolution_clock::now();
+    retrieve(da, storage);
+    auto end   = std::chrono::high_resolution_clock::now();
+    loadingTime = std::chrono::duration_cast<std::chrono::duration<double> >(end - start).count();
+
+
+    double compressionTime{0.0};
+    start = std::chrono::high_resolution_clock::now();
+    da.filterVector.compress(0); // Loading automatically compresses
+    end   = std::chrono::high_resolution_clock::now();
+    compressionTime = std::chrono::duration_cast<std::chrono::duration<double> >(end - start).count();
+
+    std::atomic<uint64_t> verifications{0};
+    std::atomic<uint64_t> tp{0};
+    std::atomic<uint64_t> p{0};
+    std::atomic<uint64_t> fp{0};
+    std::atomic<uint64_t> fn{0};
+    std::atomic<uint64_t> readNo{0};
 
     for (auto _ : state)
     {
-        double elapsed_seconds{0.0};
+        double selectTime{0.0};
+        double ioTime{0.0};
+        Semaphore thread_limiter(8);
+        std::mutex mtx;
+        std::mutex mtx2;
+        std::vector<std::future<void>> tasks;
+
         for(int32_t i = 0; i < bins; ++i)
         {
             CharString file(baseDir);
@@ -308,33 +331,76 @@ static void select_DA(benchmark::State& state)
             append(file, CharString(std::string(numDigits(bins)-numDigits(i), '0') + (std::to_string(i))));
             append(file, CharString(".fastq"));
 
-            CharString id;
-            String<TAlphabet> seq;
-            SeqFileIn seqFileIn;
-            if (!open(seqFileIn, toCString(file)))
-            {
-                CharString msg = "Unable to open contigs file: ";
-                append(msg, CharString(file));
-                throw toCString(msg);
-            }
-            while(!atEnd(seqFileIn))
-            {
-                readRecord(id, seq, seqFileIn);
-                auto start = std::chrono::high_resolution_clock::now();
-                auto res = select(da, seq, 100-k+1 - k*e);
-                auto end   = std::chrono::high_resolution_clock::now();
-                elapsed_seconds += (std::chrono::duration_cast<std::chrono::duration<double> >(end - start)).count();
+            tasks.emplace_back(
+                std::async(std::launch::async, [&, file, i] {
+                    Critical_section _(thread_limiter);
+                    CharString id;
+                    String<TAlphabet> seq;
+                    SeqFileIn seqFileIn;
+                    uint64_t c{0};
+                    if (!open(seqFileIn, toCString(file)))
+                    {
+                        CharString msg = "Unable to open contigs file: ";
+                        append(msg, CharString(file));
+                        throw toCString(msg);
+                    }
+                    while(!atEnd(seqFileIn))
+                    {
+                        auto start = std::chrono::high_resolution_clock::now();
+                        readRecord(id, seq, seqFileIn);
+                        auto end   = std::chrono::high_resolution_clock::now();
+                        mtx2.lock();
+                        ioTime += std::chrono::duration_cast<std::chrono::duration<double> >(end - start).count();
+                        mtx2.unlock();
 
-                if (res[i])
-                {
-                    ++tp;
-                }
-                verifications += count(res.begin(), res.end(), true);
-            }
+                        start = std::chrono::high_resolution_clock::now();
+                        auto res = select(da, seq, 100-k+1 - k*e);
+                        end   = std::chrono::high_resolution_clock::now();
+                        ++readNo;
+                        mtx.lock();
+                        selectTime += std::chrono::duration_cast<std::chrono::duration<double> >(end - start).count();
+                        mtx.unlock();
+
+                        if (res[i])
+                            ++tp;
+                        else
+                            ++fn;
+                        c = count(res.begin(), res.end(), true);
+                        verifications += c;
+                        if (c > 1)
+                        {
+                            if (res[i])
+                                fp += c - 1;
+                            else
+                                fp += c;
+                        }
+                        p += c;
+                    }
+                })
+            );
         }
-        state.SetIterationTime(elapsed_seconds);
-        state.counters["Verifications"] = verifications/readNo;
-        state.counters["Sensitivity"] = tp/verifications;
+        for (auto &&task : tasks){
+            task.get();
+        }
+
+        auto fullTime2   = std::chrono::high_resolution_clock::now();
+        state.counters["5_TP"] = tp.load();
+        state.counters["6_FN"] = fn.load();
+        state.counters["7_FP"] = fp.load();
+        state.counters["8_P"] = p.load();
+        state.counters["99_readNo"] = readNo.load();
+        state.counters["9_verifications"] = verifications.load();
+        state.counters["0_Verifications"] = static_cast<double>(verifications.load())/readNo.load();
+        state.counters["1_Sensitivity"] = static_cast<double>(tp.load())/readNo.load();
+        state.counters["2_Precision"] = static_cast<double>(tp.load())/p.load();
+        state.counters["3_FNR"] = static_cast<double>(fn.load())/readNo.load();
+        state.counters["4_FDR"] = static_cast<double>(fp.load())/p.load();
+        state.counters["compressionTime"] = compressionTime;
+        state.counters["loadingTime"] = loadingTime;
+        state.counters["ioTime"] = ioTime;
+        state.counters["selectTime"] = selectTime;
+        state.counters["vectorSize"] = ibf.filterVector.size_in_mega_bytes();
+        state.counters["fullTime"] = std::chrono::duration_cast<std::chrono::duration<double> >(fullTime2 - fullTime).count();
     }
 }
 
@@ -376,11 +442,11 @@ static void DAArguments(benchmark::internal::Benchmark* b)
 // BENCHMARK_TEMPLATE(insertKmer_IBF, Dna, Uncompressed)->Apply(IBFArguments);
 // BENCHMARK_TEMPLATE(insertKmer_IBF, Dna, CompressedSimple)->Apply(IBFArguments)->UseManualTime();
 // BENCHMARK_TEMPLATE(insertKmer_IBF, Dna, CompressedArray)->Apply(IBFAddArguments)->UseManualTime();
-// BENCHMARK_TEMPLATE(insertKmer_DA, Dna, Uncompressed)->Apply(DAArguments);
+BENCHMARK_TEMPLATE(insertKmer_DA, Dna, Uncompressed)->Apply(DAArguments);
 // BENCHMARK_TEMPLATE(insertKmer_DA, Dna, CompressedSimple)->Apply(DAArguments)->UseManualTime();
 // BENCHMARK_TEMPLATE(insertKmer_DA, Dna, CompressedArray)->Apply(DAAddArguments)->UseManualTime();
 // BENCHMARK_TEMPLATE(select_IBF, Dna, Uncompressed)->Apply(IBFArguments);
-BENCHMARK_TEMPLATE(select_IBF, Dna, CompressedSimple)->Apply(IBFArguments);
+// BENCHMARK_TEMPLATE(select_IBF, Dna, CompressedSimple)->Apply(IBFArguments);
 // BENCHMARK_TEMPLATE(select_IBF, Dna, CompressedArray)->Apply(IBFWhichArguments)->UseManualTime();
 // BENCHMARK_TEMPLATE(select_DA, Dna, Uncompressed)->Apply(DAArguments)->UseManualTime();
 // BENCHMARK_TEMPLATE(select_DA, Dna, CompressedSimple)->Apply(DAArguments)->UseManualTime();
