2a6584e7695d7441da9073c29da37746508c881e
Enrico Seiler <enrico.seiler@hotmail.de>
2018-06-07 09:14:26 +0200

    Edge cases for benchmark of small dataset

--- include/seqan/kmer/test/performance/g_benchmark.cpp
+++ include/seqan/kmer/test/performance/g_benchmark.cpp
@@ -413,7 +413,7 @@ static void IBFArguments(benchmark::internal::Benchmark* b)
         for (int32_t k = 14; k < 20; ++k)
         {
             // 35 = 4GiB, 36 = 8GiB, 37 = 16GiB
-            for (int32_t bits = 25; bits <= 29; ++bits )
+            for (int32_t bits = 20; bits <= 24; ++bits )
             {
                 for (int32_t hashNo = 3; hashNo < 4; ++hashNo)
                 {
@@ -432,7 +432,7 @@ static void DAArguments(benchmark::internal::Benchmark* b)
     {
         if ((binNo > 1 && binNo < 64) || binNo==128 || binNo==512 || binNo==2048 || binNo==4096)
             continue;
-        for (int32_t k = 5; k <= 9; ++k)
+        for (int32_t k = 3; k <= 4; ++k)
         {
             if (binNo == 8192 && k == 14)
                 continue;
