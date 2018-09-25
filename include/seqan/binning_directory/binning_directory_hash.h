8e65e68179e54eabd5349559e7261c868d89159e
Enrico Seiler <enrico.seiler@hotmail.de>
2018-09-25 18:58:38 +0200

    Specialise hashing for dna4

--- include/seqan/binning_directory/binning_directory_hash.h
+++ include/seqan/binning_directory/binning_directory_hash.h
@@ -101,10 +101,9 @@ public:
         return seqan::length(kmerShape);
     }
 
-    template<typename TString>
+    template<typename TString, std::enable_if_t<std::is_same<TString, String<Dna>>::value, int> = 0>
     inline std::vector<uint64_t> getHash(TString const & text)
     {
-        // std::cerr << "NORMAL\n";
         uint32_t possible = seqan::length(text) - kmerSize + 1;
 
         std::vector<uint64_t> kmerHashes(possible, 0);
@@ -122,6 +121,42 @@ public:
             ++it;
         }
 
+        return kmerHashes;
+    }
+
+    template<typename TString, std::enable_if_t<!std::is_same<TString, String<Dna>>::value, int> = 0>
+    inline std::vector<uint64_t> getHash(TString const & text)
+    {
+        uint32_t possible = seqan::length(text) - kmerSize + 1;
+
+        std::vector<uint64_t> kmerHashes(possible, 0);
+
+        uint8_t significantPos = std::ceil((double) ValueSize<TValue>::VALUE / chunks);
+
+        Shape<TValue, SimpleShape> chunkShape;
+        seqan::resize(chunkShape, significantPos);
+        uint16_t cacheKmerSize = kmerSize;
+        resize(kmerSize - significantPos);
+        auto it = begin(text) + significantPos;
+        hashInit(it);
+        auto itChunk = begin(text);
+        seqan::hashInit(chunkShape, itChunk);
+
+        for (uint32_t i = 0; i < possible; ++i)
+        {
+            uint8_t chunkIdentifier = seqan::hashNext(chunkShape, itChunk);
+
+            uint8_t chunkId = std::ceil((double) chunkIdentifier / chunks);
+
+            uint8_t chunk = std::max(0, chunkId - 1);
+
+            uint64_t chunkOffset = ipow(ValueSize<TValue>::VALUE, kmerSize - chunk) * chunk;
+            uint64_t temp = hashNext(it);
+            kmerHashes[i] = chunkOffset + temp; // hashNext(it);
+            ++it;
+            ++itChunk;
+        }
+        resize(cacheKmerSize);
         return kmerHashes;
     }
 };
@@ -188,7 +223,7 @@ public:
         return seqan::length(kmerShape);
     }
 
-    template<typename TString>
+    template<typename TString, std::enable_if_t<std::is_same<TString, String<Dna>>::value, int> = 0>
     inline std::vector<uint64_t> getHash(TString const & text)
     {
         // how many test positions are left if we take every offset'th kmer
@@ -222,6 +257,64 @@ public:
                 ++j;
             }
             ++it;
+        }
+
+        return kmerHashes;
+    }
+
+    template<typename TString, std::enable_if_t<!std::is_same<TString, String<Dna>>::value, int> = 0>
+    inline std::vector<uint64_t> getHash(TString const & text)
+    {
+        // how many test positions are left if we take every offset'th kmer
+        uint16_t x = (seqan::length(text) - kmerSize) % offset;
+        // how many kmers are there when we take every offset'th kmer
+        // possible = something left (1/0) + how many fit in the text
+        // if something is left, we add a kmer that covers these positions
+        uint32_t possible = bool(x) + (seqan::length(text) - kmerSize + offset - x) / offset;
+        uint32_t positions = seqan::length(text) - kmerSize + 1;
+
+        std::vector<uint64_t> kmerHashes(possible, 0);
+
+        uint8_t significantPos = std::ceil((double) ValueSize<TValue>::VALUE / chunks);
+
+        Shape<TValue, SimpleShape> chunkShape;
+        seqan::resize(chunkShape, significantPos);
+        uint16_t cacheKmerSize = kmerSize;
+        resize(kmerSize - significantPos);
+        auto it = begin(text) + significantPos;
+        hashInit(it);
+        auto itChunk = begin(text);
+        seqan::hashInit(chunkShape, itChunk);
+
+        for (uint32_t i = 0, j = 0; i < positions; ++i)
+        {
+            // std::cerr << "OFFSET\n";
+            uint64_t kmerHash = hashNext(it);
+            uint8_t  chunkIdentifier = seqan::hashNext(chunkShape, itChunk);
+            if (x && i == positions - 1) // we take the last kmer that covers otherwise uncovered positions
+            {
+                uint8_t chunkId = std::ceil((double) chunkIdentifier / chunks);
+
+                uint8_t chunk = std::max(0, chunkId - 1);
+
+                uint64_t chunkOffset = ipow(ValueSize<TValue>::VALUE, kmerSize - chunk) * chunk;
+
+                kmerHashes[j] = chunkOffset + kmerHash;
+                break;
+            }
+            if (i - j * offset == 0) // we found the j'th kmer with offset
+            {
+                uint8_t chunkId = std::ceil((double) chunkIdentifier / chunks);
+
+                uint8_t chunk = std::max(0, chunkId - 1);
+
+                uint64_t chunkOffset = ipow(ValueSize<TValue>::VALUE, kmerSize - chunk) * chunk;
+
+                kmerHashes[j] = chunkOffset + kmerHash;
+                ++j;
+            }
+            ++it;
+            ++itChunk;
         }
 
         return kmerHashes;
