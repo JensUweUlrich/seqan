a7100a50e4c8bdf4df7254068a2cffb12e468766
Enrico Seiler <enrico.seiler@hotmail.de>
2018-10-24 13:45:18 +0200

    Change threshold

--- include/seqan/binning_directory/binning_directory_hash.h
+++ include/seqan/binning_directory/binning_directory_hash.h
@@ -319,16 +319,43 @@ public:
 
     inline uint32_t get_threshold(uint32_t t, uint16_t e)
     {
-        get_coverage();
         (void) t;
         uint32_t destroyed{0};
+        minBegin.erase(std::unique(std::begin(minBegin), std::end(minBegin)), std::end(minBegin));
+        uint32_t available{static_cast<uint32_t>(minBegin.size())};
+
         for (uint16_t i = 0; i < e; ++i)
         {
+            get_coverage();
             auto max = std::max_element(coverage.begin(), coverage.end());
-            destroyed += *max;
-            *max = 0;
+            if (i == e - 1)
+                destroyed += *max;
+            else
+            {
+                std::vector<uint64_t> newBegin;
+                std::vector<uint64_t> newEnd;
+                newBegin.reserve(available - destroyed);
+                newEnd.reserve(available - destroyed);
+
+                auto idx = std::distance(coverage.begin(), max);
+                auto begin = minBegin[idx];
+                auto end =  minEnd[idx];
+                for (uint64_t i = 0; i < minBegin.size(); ++i)
+                {
+                    auto current = minBegin[i];
+                    if (current >= begin && current <= end)
+                        continue;
+                    newBegin.push_back(current);
+                    newEnd.push_back(minEnd[i]);
+                }
+                minBegin = std::move(newBegin);
+                minEnd = std::move(newEnd);
+                coverageBegin.clear();
+                coverageEnd.clear();
+                coverage.clear();
+            }
         }
-        return destroyed > minBegin.size() > 0 ? 0 : minBegin.size() - destroyed;
+        return destroyed > available ? 0 : available - destroyed;
     }
 
     inline void get_coverage()
